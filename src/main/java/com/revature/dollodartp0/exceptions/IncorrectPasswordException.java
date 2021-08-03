f9a3b15bc2fdcfd9df57f2e6991bfd36b144ab6f
dollodart <davidollodart@gmail.com>
2021-08-02 19:57:18 -0700

    actually add bare classes to what was empty file (fix for commit 606b2dada62cf6c5f79dc5bae47504d1cbcb68b9)

---
+++ src/main/java/com/revature/dollodartp0/exceptions/IncorrectPasswordException.java
@@ -0,0 +1,5 @@
+package com.revature.dollodartp0.exceptions;
+
+class IncorrectPasswordException extends RuntimeException {
+    // customize?
+}
---
+++ src/main/java/com/revature/dollodartp0/exceptions/InvalidTimeException.java
@@ -0,0 +1,5 @@
+package com.revature.dollodartp0.exceptions;
+
+class InvalidTimeException extends RuntimeException {
+    //
+}
