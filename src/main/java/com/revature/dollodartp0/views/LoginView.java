ac038c2ec42b333adf51faef625108d678156147
dollodart <davidollodart@gmail.com>
2021-08-03 08:15:03 -0700

    render method script for login sketched

--- src/main/java/com/revature/dollodartp0/views/LoginView.java
+++ src/main/java/com/revature/dollodartp0/views/LoginView.java
@@ -1,6 +1,8 @@
 package com.revature.dollodartp0.views;
 
+import com.revature.dollodartp0.ProjectZeroApp;
 import com.revature.dollodartp0.dao.RegistrationCatalogDAO;
+import com.revature.dollodartp0.model.User;
 
 import java.io.BufferedReader;
 
@@ -12,6 +14,19 @@ public class LoginView extends View{
 
     @Override
     public void render() {
-
+        String username = repeatedAskForUsername();
+        String password = repeatedAskForPassword();
+        User user = registrationCatalogDAO.getUserByUsernamePassword(username, password);
+        if (user == null) {
+            System.out.println("user isn't in database, try again (1) or register (2)");
+            String userInput = repeatedAsk("route");
+            if (userInput.trim().equals("1")) {
+                ProjectZeroApp.currentRoute = ProjectZeroApp.routingTable.LOGIN.route;}
+            else {
+                ProjectZeroApp.currentRoute = ProjectZeroApp.routingTable.REGISTER.route;}
+        } else {
+            System.out.println("successfully logged in");
+            // ProjectZeroApp.currentUser = user;
+        }
     }
 }
