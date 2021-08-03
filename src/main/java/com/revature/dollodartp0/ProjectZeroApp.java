27a4e4e28e3d45fac6d514984e447085830a2a68
dollodart <davidollodart@gmail.com>
2021-08-02 20:38:09 -0700

    fix to reference variables, dependency injection, and appstate switching logic

--- src/main/java/com/revature/dollodartp0/ProjectZeroApp.java
+++ src/main/java/com/revature/dollodartp0/ProjectZeroApp.java
@@ -1,19 +1,22 @@
 package com.revature.dollodartp0;
 
+import com.revature.dollodartp0.dao.RegistrationCatalogDAO;
+import com.revature.dollodartp0.views.RegistrationCatalogFacultyView;
+import com.revature.dollodartp0.views.RegistrationCatalogStudentView;
 import com.revature.dollodartp0.views.View;
 import com.revature.dollodartp0.views.LoginView;
-import com.revature.dollodartp0.dao.RegisterCatalogDAO;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ProjectZeroApp{
     /**
      You will be building a console-based student management application.
      */
     
-    public static View currentView;
+    public static String currentRoute;
     // not necessarily good practice, but don't want to dependency inject the App State
     // which is the one most global thing of anything, into every view in order to give it a callback
     private final HashMap<String, View> views = new HashMap<>();
@@ -32,21 +35,21 @@ public class ProjectZeroApp{
 
     public static void main(String[] args){
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-		RegisterCatalogDAO registerCatalogDAO = new RegisterCatalogDAO();
-		loginview = new LoginView(consoleReader, registerCatalogDAO);
-		currentScreen = loginview;
+		RegistrationCatalogDAO registerCatalogDAO = new RegistrationCatalogDAO();
+		View loginview = new LoginView(registerCatalogDAO, consoleReader);
+		View currentScreen = loginview;
 		views.add(loginview);
 		// technically this object creation could be deferred until it is known whether it is faculty or student
 		// but this is at the moment designed as eager loading
-		views.add(new RegistrationCatalogFacultyView(consoleReader, registerCatalogDAO));
-		views.add(new RegistrationCatalogStudentView(consoleReader, registerCatalogDAO));
+		views.add(new RegistrationCatalogFacultyView(registerCatalogDAO, consoleReader));
+		views.add(new RegistrationCatalogStudentView(registerCatalogDAO, consoleReader));
 
 		ProjectZeroApp appState = new ProjectZeroApp();
 		// the views render method must complete
 		// no need for concurrency and therefore threads between views
 		// since only one view is displayed at a time
 		while (true) {
- 	    	try {ProjectZeroApp(currentRoute);}
+ 	    	try {appState.switchScreen(currentRoute);}
 	    catch (Exception e) {e.printStackTrace();}
 		}
     }
