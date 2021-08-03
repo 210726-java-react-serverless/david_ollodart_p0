da454a78e834ece80170758e2084d5af3e07ad64
dollodart <davidollodart@gmail.com>
2021-08-02 20:17:46 -0700

    change method stubs and add constructor stub to use eventual mongo db querying object

--- src/main/java/com/revature/dollodartp0/dao/RegistrationCatalogDAO.java
+++ src/main/java/com/revature/dollodartp0/dao/RegistrationCatalogDAO.java
@@ -1,9 +1,12 @@
 package com.revature.dollodartp0.dao;
+import java.util.TreeSet;
 
 public class RegistrationCatalogDAO{
 
-    public List<Class> getClasses(String class_name) {
-            // List type in case class name, like ENG 101 
+    RegistrationCatalogDAO(MongoDB mongoDB) {} // constructor using backend object
+
+    public TreeSet<Class> getClasses(String class_name) {
+            // TreeSet (TODO: comparator or Class compare method for sorting query result)
     }
 
     public void addClass(String class_name) {
@@ -12,7 +15,7 @@ public class RegistrationCatalogDAO{
             // if it doesn't, then create a class
     }
 
-    public list<Class> getClassesByStudent(String student_name) {
+    public TreeSet<Class> getClassesByStudent(String student_name) {
             // used for student view (display all classes I am a part of)
     }
 
