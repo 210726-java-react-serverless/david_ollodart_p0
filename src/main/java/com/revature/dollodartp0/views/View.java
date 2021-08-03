257cedc0865f394f13bc5cd767570e428b0e0c08
dollodart <davidollodart@gmail.com>
2021-08-03 07:25:49 -0700

    user input functions

--- src/main/java/com/revature/dollodartp0/views/View.java
+++ src/main/java/com/revature/dollodartp0/views/View.java
@@ -3,6 +3,7 @@ package com.revature.dollodartp0.views;
 import com.revature.dollodartp0.dao.RegistrationCatalogDAO;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 
 abstract public class View implements ViewInterface{
 
@@ -12,5 +13,29 @@ abstract public class View implements ViewInterface{
     public View(RegistrationCatalogDAO registrationCatalogDAO, BufferedReader consoleReader) {
 	    this.registrationCatalogDAO = registrationCatalogDAO;
 	    this.consoleReader = consoleReader;
+    }
+
+    protected String repeatedAsk(String field) {
+        String userInput;
+        try {
+            System.out.printf("enter %s> ", field);
+            return consoleReader.readLine();
+        } catch (IOException io) {
+            System.out.println("\ntry again");
+            repeatedAsk(field);
+        }
+        return repeatedAsk(field); // in case not caught by IOException
+    }
+    protected String repeatedAskForUsername() {
+        return repeatedAsk("username");}
+    protected String repeatedAskForPassword() {
+        return repeatedAsk("password");}
+    protected String repeatedAskForPasswordTwice() {
+        String entry1 = repeatedAsk("password");
+        String entry2 = repeatedAsk("password again");
+        if (entry1.equals(entry2)) {return entry1;}
+        else {
+            System.out.println("try again");
+            return repeatedAskForPasswordTwice();}
     }
 }
